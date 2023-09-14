64c6237c39163054e3ef68b8979d33adc5be0f9f
domenhribernik <domen.hribernik4@gmail.com>
2023-09-14 07:22:18 +0200

    Added more photos, switched to html rendering, instead of canvas

--- ASCII-text-image/script.js
+++ ASCII-text-image/script.js
@@ -1,8 +1,32 @@
-function setup() {
-    createCanvas(400, 400);
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+let img;
+
+function preload() {
+    img = loadImage("photos/girl-pearl100.jpg");
 }
 
-function draw() {
-    background(220);
-    ellipse(200, 200, 50, 50);
+function setup() {
+    noCanvas();
+
+    background(0);
+    //image(img, 0, 0, width, height)
+
+    let w = width / img.width;
+    let h = height / img.height;
+    img.loadPixels();
+    for (let j = 0; j < img.height; j++) {
+        let row = '';
+        for (let i = 0; i < img.width; i++) {
+            const pixleIndex = (i + j * img.width) * 4;
+            const r = img.pixels[pixleIndex + 0];
+            const g = img.pixels[pixleIndex + 1];
+            const b = img.pixels[pixleIndex + 2];
+            const avg = (r+g+b) / 3;
+            const len = density.length;
+            const charIndex = floor(map(avg,0,255,len,0));
+
+            row += density.charAt(charIndex);
+        }
+        createDiv(row);
+    }
 }
